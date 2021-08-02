1454067e85671c1828fe6202bc099741e9d97e19
Igor Baiocco <igor64br@gmail.com>
2021-08-02 17:05:33 -0300

    v1.1.1 - Code organization

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
-
 from tkinter import *
 from pygame import mixer
 from math import floor
 # ---------------------------- CONSTANTS ------------------------------- #
+music_file_location = '/home/baiocco/a100DoC_projects/day-28-pomodoro_counter/iPhone_radar.wav'
+# OBS.: Go to your alarm sound's properties and copy and paste it's location above
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#346751"
@@ -20,7 +21,7 @@ timer = None
 # ---------------------------- Break Sound ------------------------------- #
 def play_alarm():
     mixer.init()
-    mixer.music.load('iPhone_radar.wav')
+    mixer.music.load(music_file_location)
     mixer.music.play(loops=0)
 
 
@@ -39,9 +40,9 @@ def mechanism():
     global reps
     reps += 1
 
-    work_sec = WORK_MIN #* 60
-    short_br_sec = SHORT_BREAK_MIN #* 60
-    long_br_sec = LONG_BREAK_MIN #* 60
+    work_sec = WORK_MIN * 60
+    short_br_sec = SHORT_BREAK_MIN * 60
+    long_br_sec = LONG_BREAK_MIN * 60
 
     if reps % 8 == 0:
         count_down(long_br_sec)
@@ -82,7 +83,7 @@ def count_down(count):
 
 
 def start_count_down():
-    count = WORK_MIN * 60
+    # count = WORK_MIN * 60
     mechanism()
 
 
@@ -105,7 +106,7 @@ checkmarks = Label(text="", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
 checkmarks.grid(column=2, row=4)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_pic = PhotoImage(file="tomato.png")
+tomato_pic = PhotoImage(file="/home/baiocco/a100DoC_projects/day-28-pomodoro_counter/tomato.png")
 canvas.create_image(100, 112, image=tomato_pic)
 counter_text = canvas.create_text(100, 140, text="0:00", fill="white", font=(FONT_NAME, 40, "bold"))
 canvas.grid(column=2, row=2)
